2fe4942bae45206c85400968f12a9b051572f4e6
Sky Chen <sky.chen@salesforce.com>
2017-08-15 02:03:53 -0700

    use different method to deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,18 +7,18 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@192.241.204.199:/var/www/ai-tester.com"
-    git config user.name "Travis CI"
-    git config user.email "travis@ai-tester.com"
+    # ssh-add ~/.ssh/travis_rsa
+    ssh-add ~/.ssh/id_rsa
+    ssh almandsky@192.241.204.199 './deploy.sh'
+    # git remote add deploy "travis@192.241.204.199:/var/www/ai-tester.com"
+    # git config user.name "Travis CI"
+    # git config user.email "travis@ai-tester.com"
 
-    # commit compressed files and push it to remote
-    # rm -f .gitignore
-    # cp .travis/deployignore .gitignore
-    git add .
-    git status # debug
-    git commit -m "Deploy compressed files"
-    git push -f deploy HEAD:master
+    # # commit compressed files and push it to remote
+    # git add .
+    # git status # debug
+    # git commit -m "Deploy compressed files"
+    # git push -f deploy HEAD:master
 
 elif [ $TRAVIS_BRANCH == "staging" ] ; then
 
